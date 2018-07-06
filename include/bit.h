2d175e45f58fb9094608ba46cc87c18f5e5015a3
chuangix <chuangix@qq.com>
2018-07-06 16:35:46 +0800

    TODO: Add some macro functions
    TODO: BIT_FIND_0()
    TODO: BIT_FIND_1()
    TODO: BIT_FIND_DIFF()
    TODO: BIT_FIND_IDEN()
    TODO: BIT_SEARCH_0()
    TODO: BIT_SEARCH_1()
    TODO: BIT_SEARCH_DIFF()
    TODO: BIT_SEARCH_IDEN()
    TODO: BIT_SET()
    TODO: BIT_CLR()
    TODO: BIT_REV()
    TODO: BIT_SET_Nth()
    TODO: BIT_CLR_Nth()
    TODO: BIT_REV_Nth()
    TODO: BIT_INV()
    TODO: BIT_ENDIAN_SWAP()

--- include/bit.h
+++ include/bit.h
@@ -3,6 +3,38 @@
 
 #define LITTLE_ENDIAN
 
+#define BIT_FIND_0(x)
+
+#define BIT_FIND_1(x)
+
+#define BIT_FIND_DIFF(x)
+
+#define BIT_FIND_IDEN(x)
+
+#define BIT_SEARCH_0(x)
+
+#define BIT_SEARCH_1(x)
+
+#define BIT_SEARCH_DIFF(x)
+
+#define BIT_SEARCH_IDEN(x)
+
+#define BIT_SET(x, mask)
+
+#define BIT_CLR(x, mask)
+
+#define BIT_REV(x, mask)
+
+#define BIT_SET_Nth(x, n)
+
+#define BIT_CLR_Nth(x, n)
+
+#define BIT_REV_Nth(x, n)
+
+#define BIT_INV(x)
+
+#define BIT_ENDIAN_SWAP(x)
+
 #ifdef LITTLE_ENDIAN
 #define BIT_GET_SIGN(x) \
         (((signed char *)&x)[sizeof(x) - 1] >> 7 | 1)
